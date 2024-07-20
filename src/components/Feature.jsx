0270dc4b865b3150e24078f272dfaaa4a5059412
FransiskoLpz <fbperez31@gmail.com>
2024-07-19 21:40:51 -0600

    prueba2

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -9,6 +9,15 @@ function Feature() {
   return (
     <div id="features">
       <h1>About Us</h1>
+      <p>
+        Welcome to Zamora's Heavy Hitters, your premier boxing gym in Port St.
+        Lucie. At Zamora's Heavy Hitters, we are dedicated to providing a
+        challenging and supportive environment for boxers of all levels. Our
+        expert trainers, state-of-the-art facilities and inclusive community
+        will help you improve your skills, improve your fitness and build
+        confidence. Join us to experience the transformative power of boxing and
+        become part of our passionate and encouraging family.
+      </p>
       <div className="a-container">
         <Featurebox image={fimage1} title="WeightLifting" />
         <Featurebox image={fimage2} title="Specific Muscle" />
